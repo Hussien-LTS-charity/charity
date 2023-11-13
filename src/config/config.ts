ddea2c24be728724361ee61c88b7546e4aab2587
Hussien-LTS <hussain662536@gmail.com>
2023-11-13 11:56:50 +0300

    fixing the github_actions run tests step

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,21 +2,21 @@ import dotenv from "dotenv";
 dotenv.config();
 export const databaseConfig = {
   development: {
-    database: process.env.DB_DEV_NAME,
-    username: process.env.DB_DEV_USER,
-    password: process.env.DB_DEV_PASS,
+    database: "ynnbevvc",
+    username: "ynnbevvc",
+    password: "sYVxsUAck-Ik1uvNl6GY6e6r98FPdTdF",
     host: "peanut.db.elephantsql.com",
   },
   test: {
-    database: process.env.DB_TEST_NAME,
-    username: process.env.DB_TEST_USER,
-    password: process.env.DB_TEST_PASS,
+    database: "acgnczas",
+    username: "acgnczas",
+    password: "ouBWh79Wadgxj0Tlyc8KftOTqkBHt2yJ",
     host: "peanut.db.elephantsql.com",
   },
   production: {
-    database: process.env.DB_PROD_NAME,
-    username: process.env.DB_PROD_USER,
-    password: process.env.DB_PROD_PASS,
+    database: "zlyachnp",
+    username: "zlyachnp",
+    password: "U2Jk9BFL6R67b0ir4NKXZ6gREeRoqkiw",
     host: "peanut.db.elephantsql.com",
   },
 };
