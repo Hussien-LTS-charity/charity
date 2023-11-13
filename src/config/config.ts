3963da432ea6bc43f97eb73512e60e2e84170c7c
Hussien-LTS <hussain662536@gmail.com>
2023-11-13 12:19:53 +0300

    fixing the github_actions run tests step

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,21 +2,15 @@ import dotenv from "dotenv";
 dotenv.config();
 export const databaseConfig = {
   development: {
-    database: "ynnbevvc",
-    username: "ynnbevvc",
-    password: "sYVxsUAck-Ik1uvNl6GY6e6r98FPdTdF",
-    host: "peanut.db.elephantsql.com",
-  },
-  test: {
-    database: "acgnczas",
-    username: "acgnczas",
-    password: "ouBWh79Wadgxj0Tlyc8KftOTqkBHt2yJ",
-    host: "peanut.db.elephantsql.com",
+    database: process.env.DB_DEV_NAME,
+    username: process.env.DB_DEV_USER,
+    password: process.env.DB_DEV_PASS,
+    host: process.env.DB_HOST,
   },
   production: {
-    database: "zlyachnp",
-    username: "zlyachnp",
-    password: "U2Jk9BFL6R67b0ir4NKXZ6gREeRoqkiw",
-    host: "peanut.db.elephantsql.com",
+    database: process.env.DB_PROD_NAME,
+    username: process.env.DB_PROD_USER,
+    password: process.env.DB_PROD_PASS,
+    host: process.env.DB_HOST,
   },
 };
--- src/config/types.ts
+++ src/config/types.ts
@@ -8,7 +8,7 @@ import {
   Priority,
   monyDonationsSource,
 } from "./enums";
-export type environmentAttributes = "development" | "test" | "production";
+export type environmentAttributes = "development" | "production";
 
 export interface FamilyAttributes {
   id: number;
